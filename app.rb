0dc32b1f6008342758b840b015da91ef1b44db44
Yefim Vedernikoff <yefim323@gmail.com>
2020-09-08 18:04:11 -0700

    Add edit

--- app.rb
+++ app.rb
@@ -2,9 +2,35 @@ require 'sinatra'
 require 'redis'
 
 get '/' do
-  if request.host.include?('dance.yef.im')
-    redirect 'https://google.com'
+  subdomain = request.host.split('.').first
+  redirect_url = redis.get(subdomain)
+
+  if redirect_url
+    redirect redirect_url
   else
     "happy birthday yefim 🥳 You're at #{request.host}"
   end
+end
+
+get '/edit' do
+  %q(
+    <form method="post" action="">
+      <label>Redirect URL
+        <input type="text" name="redirect_url">
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  )
+end
+
+post '/edit' do
+  subdomain = request.host.split('.').first
+  redirect_url = params['redirect_url']
+
+  redis.set(subdomain, redirect_url)
+  redirect redirect_url
+end
+
+def redis
+  @redis ||= Redis.new(url: ENV["REDIS_URL"])
 end
